54407c5a1e6b2a1d834baa1c73e50022f206cce0
Andrielly Lucena <andrielly.lucena@ccc.ufcg.edu.br>
2020-04-02 16:56:53 -0300

    código mobile

---
+++ App.js
@@ -0,0 +1,14 @@
+import 'intl';
+import 'intl/locale-data/jsonp/pt-BR';
+
+import React from 'react';
+
+import Routes from './src/routes';
+// as tags nao tem semantica
+// nao existe herança de estilo
+
+export default function App() {
+  return (
+      <Routes/>
+  );
+}
